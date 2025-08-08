a262f438729e582370ebc8f9e84a192ee05d9808
ferdie bergado <ferdiebergado@gmail.com>
2025-08-08 21:45:23 +0800

    feat: Add global loading indicator

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout() {
+    const navigation = useNavigation()
     return (
         <>
             <Header />
             <main>
                 <section>
-                    <Outlet />
+                    {navigation.state === 'loading' ? (
+                        <p className="mt-auto py-6 text-center">Loading...</p>
+                    ) : (
+                        <Outlet />
+                    )}
                 </section>
             </main>
             <Footer />
