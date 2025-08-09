c041f429d57a5da78f1dd81c7ea55403da4df6a1
ferdie bergado <ferdiebergado@gmail.com>
2025-08-09 21:43:14 +0800

    refactor: Justify and italicize text

--- src/features/bible/Verse.tsx
+++ src/features/bible/Verse.tsx
@@ -3,13 +3,18 @@ import { Card, CardContent, CardTitle } from '@/components/ui/card'
 interface VerseProps {
     verse: number
     text: string
+    book?: string
+    chapter?: number
 }
 
-export default function Verse({ verse, text }: VerseProps) {
+export default function Verse({ verse, text, book, chapter }: VerseProps) {
     return (
-        <Card className="my-2 w-full max-w-lg px-4 py-3">
-            <CardTitle>Verse {verse}</CardTitle>
-            <CardContent>{text}</CardContent>
+        <Card className="my-3 w-full max-w-lg px-4 py-4">
+            <CardTitle>
+                {book ? `${book} ` : ''}
+                {chapter ? `Chapter ${chapter.toString()} ` : ''}Verse {verse}
+            </CardTitle>
+            <CardContent className="text-justify italic">{text}</CardContent>
         </Card>
     )
 }
