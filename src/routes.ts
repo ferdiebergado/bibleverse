f76852f87ea3dd919a196afaa951a96776131b90
ferdie bergado <ferdiebergado@gmail.com>
2025-08-10 10:44:23 +0800

    refactor: Add HydrateFallback

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import type { RouteObject } from 'react-router'
 import ErrorBoundary from './components/ErrorBoundary'
 import Layout from './components/Layout'
 import NotFound from './components/NotFound'
+import Spinner from './components/Spinner'
 import queryClient from './lib/queryClient'
 
 export const routes: RouteObject[] = [
@@ -9,6 +10,7 @@ export const routes: RouteObject[] = [
         path: '/',
         Component: Layout,
         ErrorBoundary: ErrorBoundary,
+        HydrateFallback: Spinner,
         children: [
             {
                 index: true,
