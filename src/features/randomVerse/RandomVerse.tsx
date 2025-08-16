c7efc36a74dd340023d46dde9adabb2eea3674a8
ferdie bergado <ferdiebergado@gmail.com>
2025-08-16 14:09:21 +0800

    refactor: Adjust padding to align with search card

--- src/features/randomVerse/RandomVerse.tsx
+++ src/features/randomVerse/RandomVerse.tsx
@@ -6,8 +6,8 @@ export default function RandomVerse() {
         useLoaderData<Awaited<ReturnType<typeof randomVerseLoader>>>()
 
     return (
-        <div className="m-8 flex w-full flex-col items-center">
-            <blockquote className="max-w-4xl px-4.5 text-justify text-xl italic">
+        <div className="flex w-full flex-col items-center">
+            <blockquote className="max-w-4xl px-16 text-justify text-xl italic">
                 {text}
                 <footer className="my-4 text-2xl font-bold">
                     {`${book} ${chapter.toString()}:${verse.toString()}`}
