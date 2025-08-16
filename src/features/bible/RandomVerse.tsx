6588bcfbe1e6c31f643c8c0221e6483e5219ca01
ferdie bergado <ferdiebergado@gmail.com>
2025-08-16 13:40:41 +0800

    refactor: Align blockquote footer to the left

--- src/features/bible/RandomVerse.tsx
+++ src/features/bible/RandomVerse.tsx
@@ -6,10 +6,10 @@ export default function RandomVerse() {
         useLoaderData<Awaited<ReturnType<typeof randomVerseLoader>>>()
 
     return (
-        <div className="flex w-full flex-col items-center">
-            <blockquote className="max-w-4xl px-4.5 text-xl italic">
+        <div className="m-8 flex w-full flex-col items-center">
+            <blockquote className="max-w-4xl px-4.5 text-justify text-xl italic">
                 {text}
-                <footer className="my-4 text-right text-2xl font-bold">
+                <footer className="my-4 text-2xl font-bold">
                     {`${book} ${chapter.toString()}:${verse.toString()}`}
                 </footer>
             </blockquote>
