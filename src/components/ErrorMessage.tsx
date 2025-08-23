0c12b5df02cc97318e3e2b0ce2d6b3b5485d658b
ferdie bergado <ferdiebergado@gmail.com>
2025-08-23 19:27:37 +0800

    refactor: Use destructive variable for text color

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react'
 
 interface ErrorMessageProps {
-    message: string
+    error: Error
 }
 
-const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
-    return <p className="text-red-500">Error: {message}</p>
+const ErrorMessage: FC<ErrorMessageProps> = ({ error: { message } }) => {
+    return <p className="text-destructive">Error: {message}</p>
 }
 
 export default ErrorMessage
